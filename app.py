4af1109b39d408e491697b5ba5502eee18cd95a4
Elvan Selvano <elvan@xyz.lan>
2024-05-12 19:31:28 +0700

    Add audiorecorder

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import streamlit as st
+from audiorecorder import audiorecorder
 
 st.set_page_config(
-    page_title="Starting App",
+  page_title="Starting App",
 )
 
 def main():
-    st.markdown("<br>", unsafe_allow_html=True)
-    st.title("Starting app")
+  st.markdown("<br>", unsafe_allow_html=True)
+  st.title("Starting app")
+
+  st.title("Audio Recorder")
+  audio = audiorecorder(start_prompt="", stop_prompt="", pause_prompt="", show_visualizer=True)
+
+  if len(audio) > 0:
+      st.audio(audio.export().read())
+
+      audio.export("audio.wav", format="wav")
+
+      st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
+
+def test_main():
+  pass
 
 if __name__ == "__main__":
-    main()
+  main()

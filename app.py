5e3740f2021061c76e660716679049b73b66365d
Roland <felixroland12355@gmail.com>
2024-05-12 21:22:20 +0700

    feature: transcribe to text
    transcribe from audio to text initial success. Some errors in detection, may be the result of wrong audio conversion

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from audiorecorder import audiorecorder
+import whisper
+import numpy as np
 
 st.set_page_config(
     page_title="Starting App",
@@ -7,6 +10,12 @@ st.set_page_config(
 def main():
     st.markdown("<br>", unsafe_allow_html=True)
     st.title("Starting app")
+    audio = audiorecorder("Click to record", "Click to stop recording")
+
+    if len(audio) > 0:
+        model = whisper.load_model("base")
+        result = model.transcribe(np.frombuffer(audio.raw_data, np.int16).flatten().astype(np.float32) / 32768.0)
+        st.write(result['text'])
 
 if __name__ == "__main__":
     main()

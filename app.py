bee256ff4ee9c99f57b9b2524d4908967ce3a1b4
Dody Harianto <71028982+dodyharianto@users.noreply.github.com>
2024-05-19 13:14:16 +0700

    Update app.py
    Use cache to store loaded Whisper model.

--- app.py
+++ app.py
@@ -2,22 +2,37 @@ import streamlit as st
 from audiorecorder import audiorecorder
 import whisper
 import numpy as np
+from pydub import AudioSegment
+import librosa
 
 st.set_page_config(
     page_title="Starting App",
 )
 
+@st.cache
+def load_whisper_model():
+    model = whisper.load_model("medium")
+    return model
+
 def main():
     st.markdown("<br>", unsafe_allow_html=True)
     st.title("Starting app")
-    audio = audiorecorder("Click to record", "Click to stop recording")
 
+    model = load_whisper_model()
+    st.success('Whisper model loaded.')
+
+    audio = audiorecorder("Click to record", "Click to stop recording")
     if len(audio) > 0:
-        model = whisper.load_model("base")
+        audio.export("audio.mp3", format="mp3")
+        st.success("Recording saved. Now transcribing audio...")
+
+        y, _ = librosa.load('audio.mp3')
         language = 'Indonesian'
         options = dict(language=language, beam_size=5, best_of=5)
         transcribe_options = dict(task="transcribe", **options)
-        result = model.transcribe(np.frombuffer(audio.raw_data, np.int16).flatten().astype(np.float32) / 32768.0, **transcribe_options)
+        result = model.transcribe(y, **transcribe_options)
+
+        st.write('Transcribed audio:')
         st.write(result['text'])
 
 if __name__ == "__main__":

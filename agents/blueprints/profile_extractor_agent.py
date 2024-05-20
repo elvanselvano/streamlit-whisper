6b8c2840585bcf809912d01d4387682af9ca2894
Roland <felixroland12355@gmail.com>
2024-05-20 22:22:13 +0700

    format: reformat existing code using auto formatter

--- agents/blueprints/profile_extractor_agent.py
+++ agents/blueprints/profile_extractor_agent.py
@@ -1,4 +1,5 @@
 import os
+
 from haystack import Pipeline
 from haystack.components.others import Multiplexer
 from haystack.components.builders import PromptBuilder
@@ -7,18 +8,19 @@ from haystack_integrations.components.generators.ollama import OllamaGenerator
 
 def profile_extractor(pipeline_kwargs={}):
     template = ""
-    with open('agents/templates/profile_extractor_template.txt') as f:
+    with open("agents/templates/profile_extractor_template.txt") as f:
         template = f.read()
     extractor = Pipeline(**pipeline_kwargs)
-    extractor.add_component('prompt', Multiplexer(str))
-    extractor.add_component('builder', PromptBuilder(template))
-    extractor.add_component('generator', OllamaGenerator(
-        model=os.getenv('LLM_MODEL'),
-        url=str(os.getenv("LLM_URL")),
-        timeout=int(os.getenv("TIMEOUT", 10*60))
-    ))
-    extractor.connect('prompt.value', 'builder.story')
-    extractor.connect("builder", 'generator')
+    extractor.add_component("prompt", Multiplexer(str))
+    extractor.add_component("builder", PromptBuilder(template))
+    extractor.add_component(
+        "generator",
+        OllamaGenerator(
+            model=os.getenv("LLM_MODEL"),
+            url=str(os.getenv("LLM_URL")),
+            timeout=int(os.getenv("TIMEOUT", 10 * 60)),
+        ),
+    )
+    extractor.connect("prompt.value", "builder.story")
+    extractor.connect("builder", "generator")
     return extractor
-    
-    

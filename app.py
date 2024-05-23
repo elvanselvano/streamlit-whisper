8bcfb1fd05648c6f512147bb34d65cb52e694c69
Dody Harianto <71028982+dodyharianto@users.noreply.github.com>
2024-05-23 16:37:31 +0700

    Update app.py
    Convert the recorded audio to Numpy array directly.

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ import streamlit as st
 from audiorecorder import audiorecorder
 import whisper
 import numpy as np
-from pydub import AudioSegment
-import librosa
 
 st.set_page_config(
     page_title="Starting App",
 )
 
-@st.cache
+@st.cache_resource
 def load_whisper_model():
     model = whisper.load_model("medium")
     return model
@@ -23,14 +21,20 @@ def main():
 
     audio = audiorecorder("Click to record", "Click to stop recording")
     if len(audio) > 0:
-        audio.export("audio.mp3", format="mp3")
+        if audio.frame_rate != 16000:
+            audio = audio.set_frame_rate(16000)
+        if audio.sample_width != 2:
+            audio = audio.set_sample_width(2)
+        if audio.channels != 1:
+            audio = audio.set_channels(1)
+        audio_array = np.array(audio.get_array_of_samples())
+        audio_array = audio_array.astype(np.float32) / 32768.0
         st.success("Recording saved. Now transcribing audio...")
 
-        y, _ = librosa.load('audio.mp3')
         language = 'Indonesian'
         options = dict(language=language, beam_size=5, best_of=5)
         transcribe_options = dict(task="transcribe", **options)
-        result = model.transcribe(y, **transcribe_options)
+        result = model.transcribe(audio_array, **transcribe_options)
 
         st.write('Transcribed audio:')
         st.write(result['text'])

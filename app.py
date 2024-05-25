df8edf0c952d2b5892d956d74d7df64d4833ba0d
Roland <felixroland12355@gmail.com>
2024-05-25 21:57:01 +0700

    change: remove currently unused import

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from dotenv import load_dotenv
 from agents.blueprints import profile_extractor
-from audiorecorder import audiorecorder
 
 st.set_page_config(
     page_title="Starting App",
